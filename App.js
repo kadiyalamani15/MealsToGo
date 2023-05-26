81efeace41585f6066b7aa9f20b13e01675a595e
kadiyalamani15 <manishchandra.kadiyala@learner.manipal.edu>
2023-05-27 00:00:38 +0530

    Search Bar is added on Restaurant Screens

--- App.js
+++ App.js
@@ -1,35 +1,12 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
+import { RestuarantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Text>Search</Text>
-        </View>
-        <View style={styles.list}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
+      <RestuarantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-    backgroundColor: "green",
-  },
-  list: {
-    padding: 16,
-    backgroundColor: "blue",
-    flexGrow: 1,
-  },
-});
