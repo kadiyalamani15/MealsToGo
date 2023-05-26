71ade5a0b6bcff62be0b2db48650875b8522c31c
kadiyalamani15 <manishchandra.kadiyala@learner.manipal.edu>
2023-05-26 22:30:33 +0530

    ESlint config and add react-native-paper

--- App.js
+++ App.js
@@ -1,30 +1,35 @@
-import { StatusBar as ExpoStatusBar} from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
 
 export default function App() {
   return (
-		<>
-			<SafeAreaView style={{ flex: 1 , marginTop: StatusBar.currentHeight }}>
-				<View style={styles.search}>
-					<Text>Search</Text>
-				</View>
-				<View style={styles.list}>
-					<Text>List</Text>
-				</View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text>List</Text>
+        </View>
       </SafeAreaView>
       <ExpoStatusBar style="auto" />
-		</>
-	);
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
   search: {
     padding: 16,
-    backgroundColor: 'green',
+    backgroundColor: "green",
   },
   list: {
     padding: 16,
-    backgroundColor: 'blue',
+    backgroundColor: "blue",
     flexGrow: 1,
-  }
+  },
 });
