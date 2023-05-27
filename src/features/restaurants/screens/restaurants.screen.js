4578dad7dc8c91ac55f1230907784d9e405174f1
kadiyalamani15 <manishchandra.kadiyala@learner.manipal.edu>
2023-05-27 18:32:09 +0530

    Theme Setup

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,31 +1,29 @@
-import React from "react";
+import styled from "styled-components/native";
 import { Searchbar } from "react-native-paper";
 import { StatusBar, StyleSheet, View, SafeAreaView } from "react-native";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
-export const RestuarantsScreen = () => (
-  <>
-    <SafeAreaView style={styles.container}>
-      <View style={styles.search}>
-        <Searchbar />
-      </View>
-      <View style={styles.list}>
-        <RestaurantInfoCard />
-      </View>
-    </SafeAreaView>
-  </>
-);
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}`};
+`;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-  },
-});
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+export const RestuarantsScreen = () => (
+  <SafeArea>
+    <SearchContainer>
+      <Searchbar />
+    </SearchContainer>
+    <RestaurantListContainer>
+      <RestaurantInfoCard />
+    </RestaurantListContainer>
+  </SafeArea>
+);
